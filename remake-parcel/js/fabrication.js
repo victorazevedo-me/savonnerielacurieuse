c1ffed2f045cbe66849f7d6d5b7873b861dbe9d1
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-09-06 22:01:46 +0200

    :tada: Typescript! :bug: Id on right heading

--- remake-parcel/js/fabrication.js
+++ remake-parcel/js/fabrication.js
@@ -1,87 +1,62 @@
-
+"use strict";
+exports.__esModule = true;
 //toute les fct pour fabrication
 function fabricationScroll() {
-
     //init consts
-    const etapdesc = document.getElementById('etape-desc');
-    const imgwrap = document.getElementById('imgwrap');
-    const allimgs = imgwrap.querySelectorAll('img');
-    const maxcount = imgwrap.querySelectorAll('img').length
-    let count = 0
-    let lastcount = 0
-
-
+    var etapdesc = document.getElementById('etape-desc');
+    var innerTimer = document.querySelector('#timer .inner');
+    var imgwrap = document.getElementById('imgwrap');
+    var allimgs = imgwrap.querySelectorAll('img');
+    var maxcount = imgwrap.querySelectorAll('img').length;
+    var count = 0;
+    var lastcount = 0;
     function imgscroll() {
-
-        let width = imgwrap.children[0].offsetWidth
-        let margin = imgwrap.offsetWidth * .06 // a changer si la marge des img change
-        let toscroll = (width + margin) * count
-
+        var width = imgwrap.children[0].offsetWidth;
+        var margin = imgwrap.offsetWidth * .06; // a changer si la marge des img change
+        var toscroll = (width + margin) * count;
         //scroll
-        imgwrap.style.transform = `translateX(${-toscroll}px)`;
-
+        imgwrap.style.transform = "translateX(" + -toscroll + "px)";
         //focus
-        imgwrap.children[count].className = "focused"
-        imgwrap.children[lastcount].className = ""
+        imgwrap.children[count].className = "focused";
+        imgwrap.children[lastcount].className = "";
     }
-
     function setMaxDescHeight() {
-
-        let maxHeight = 0
-
-        etapdesc.querySelectorAll('p').forEach(item => {
-        if (item.offsetHeight > maxHeight)
-            maxHeight = item.offsetHeight
-        })
-
-        etapdesc.style.height = maxHeight + "px"
+        var maxHeight = 0;
+        etapdesc.querySelectorAll('p').forEach(function (item) {
+            if (item.offsetHeight > maxHeight)
+                maxHeight = item.offsetHeight;
+        });
+        etapdesc.style.height = maxHeight + "px";
     }
-
     function descScroll() {
-
-        const parafs = etapdesc.querySelectorAll('p')
-
-        parafs[lastcount].style.opacity = 0
-        parafs[lastcount].style.zIndex = 4
-        parafs[count].style.opacity = 1
-        parafs[count].style.zIndex = 5
+        var parafs = etapdesc.querySelectorAll('p');
+        parafs[lastcount].style.opacity = "0";
+        parafs[lastcount].style.zIndex = "4";
+        parafs[count].style.opacity = "1";
+        parafs[count].style.zIndex = "5";
     }
-
-
     function defileControl(next) {
-
         //suivant ou non ?
         //si length-1 alors oui, sinon precedent ?
         //si >0 alors oui 
-
-        lastcount = count
-        count += (next && count < maxcount - 1 ? 1 : (count > 0 ? -1 : 0))
-
-        imgscroll()
-        descScroll()
+        lastcount = count;
+        count += (next && count < maxcount - 1 ? 1 : (count > 0 ? -1 : 0));
+        imgscroll();
+        descScroll();
     }
-
- 
     //défile auto par defaut
-    let autoDefil = setInterval(function() {
-        defileControl(true)
-    }, 8000)
-
-    allimgs.forEach((item, index) => {
-
-        item.onclick = function() {
-
-                clearInterval(autoDefil)
-
-            
-            document.querySelector('#timer .inner').style.opacity = "0";
-            
-            (this.nextElementSibling && this.nextElementSibling.className === "focused" ? defileControl() : defileControl(true))
-        }
-    })
-
+    var autoDefil = setInterval(function () {
+        defileControl(true);
+    }, 8000);
+    allimgs.forEach(function (item) {
+        item.onclick = function () {
+            clearInterval(autoDefil);
+            innerTimer.style.opacity = "0";
+            var voisin = item.nextElementSibling || false;
+            defileControl(voisin && voisin.className === "focused" ? false : true);
+        };
+    });
     //initial functions
-    setMaxDescHeight()
+    setMaxDescHeight();
 }
-
-export default fabricationScroll
+exports["default"] = fabricationScroll;
---
+++ remake-parcel/js/fabrication.ts
@@ -0,0 +1,87 @@
+
+//toute les fct pour fabrication
+function fabricationScroll() {
+
+    //init consts
+    const etapdesc = document.getElementById('etape-desc')! as HTMLElement;
+    const innerTimer = document.querySelector('#timer .inner')! as HTMLParagraphElement;
+    const imgwrap = document.getElementById('imgwrap')! as HTMLElement;
+    const allimgs = imgwrap.querySelectorAll('img')!;
+    const maxcount = imgwrap.querySelectorAll('img').length;
+    let count = 0
+    let lastcount = 0
+
+
+    function imgscroll() {
+
+        let width = imgwrap.children[0].offsetWidth
+        let margin = imgwrap.offsetWidth * .06 // a changer si la marge des img change
+        let toscroll = (width + margin) * count
+
+        //scroll
+        imgwrap.style.transform = `translateX(${-toscroll}px)`;
+
+        //focus
+        imgwrap.children[count].className = "focused"
+        imgwrap.children[lastcount].className = ""
+    }
+
+    function setMaxDescHeight() {
+
+        let maxHeight = 0
+
+        etapdesc.querySelectorAll('p').forEach(item => {
+        if (item.offsetHeight > maxHeight)
+            maxHeight = item.offsetHeight
+        })
+
+        etapdesc.style.height = maxHeight + "px"
+    }
+
+    function descScroll() {
+
+        const parafs = etapdesc.querySelectorAll('p')
+
+        parafs[lastcount].style.opacity = "0"
+        parafs[lastcount].style.zIndex = "4"
+        parafs[count].style.opacity = "1"
+        parafs[count].style.zIndex = "5"
+    }
+
+
+    function defileControl(next: boolean) {
+
+        //suivant ou non ?
+        //si length-1 alors oui, sinon precedent ?
+        //si >0 alors oui 
+
+        lastcount = count
+        count += (next && count < maxcount - 1 ? 1 : (count > 0 ? -1 : 0))
+
+        imgscroll()
+        descScroll()
+    }
+
+ 
+    //défile auto par defaut
+    let autoDefil = setInterval(function() {
+        defileControl(true)
+    }, 8000)
+
+    allimgs.forEach(item => {
+
+        item.onclick = function() {
+
+            clearInterval(autoDefil)
+            innerTimer.style.opacity = "0";
+
+            let voisin = item.nextElementSibling || false;
+            defileControl(voisin && voisin.className === "focused" ? false : true)
+        }
+    })
+
+    //initial functions
+    setMaxDescHeight()
+}
+
+export default fabricationScroll
