a5eec3c3bdf16570aae16559e9b6b05b9861c152
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-09-22 22:29:32 +0200

    :construction: Background slides

--- remake-parcel/src/scripts/index.ts
+++ remake-parcel/src/scripts/index.ts
@@ -1,84 +1,110 @@
-
 import fabricationScroll from './fabrication';
 import Hammer from 'hammerjs';
 
 
-
-
-
-
-
-
 function openmenu() {
-    
-// const page = window.location.pathname
-// document.body.addEventListener('wheel', function(ev: any) {
 
-//     if (ev.wheelDelta < 0) {
-//         console.log('transition')
-//     }
+    // const page = window.location.pathname
+    // document.body.addEventListener('wheel', function(ev: any) {
 
-// })
+    //     if (ev.wheelDelta < 0) {
+    //         console.log('transition')
+    //     }
+
+    // })
 
 }
 
 type Directions = {
-    left: string;
-    right: string;
+    left?: string;
+    right?: string;
 }
 
- function swipes(dir: Directions): void {
+function moveBackgrounds(which : ("left" | "right")): void {
 
-    const accueil = document.body!;
-    const mc = new Hammer(accueil);
+    // deux: "current, aside"
+    // panleft -> choose right
+    // panright -> choose left
 
-    const addEvent = (which: ("left" | "right")) => {
+    //init vals
+    const wrapper = document.querySelector('.backgrounds-wrapper')!;
+    const current = wrapper.querySelector('.current')!;
+    const index = parseInt(wrapper.id);
+
+    //init aside params
+    const bckgs = ['un', 'deux', 'trois', 'quatre']
+
+    //swipe must be in backgrounds length bounds
+    if (which === "left" && index < (bckgs.length - 1) ||
+        which !== "left" && index > 0) {
+        
+        // createAside
+        const aside = document.createElement("div");
+
+        //update style & wrapper id
+        if (which === "left") {
+            wrapper.id = (index + 1).toString()
+            aside.style.position = "100%"
+            aside.className = "background aside " + bckgs[index + 1]
+            
+        } else {
+            wrapper.id = (index - 1).toString(),
+            aside.style.position = "-100%"
+            aside.className = "background aside " + bckgs[index - 1]
+        }
+
+        wrapper.insertBefore(aside, current);
+        
+        //isMoving pour controler animations
+        isMoving = true
+
+        //apply animation
+        aside.style.animation = (which === "left" ? "SlideLeft 1s" : "SlideRight 1s");
+
+        //reset animation controle
+        setTimeout(() => {
+            current.remove()
+            aside.className = "background current " + bckgs[parseInt(wrapper.id)]
+            isMoving = false
+        }, 1000)
+    }
+}
+
+
+function swipes(dir? : Directions): void {
+
+    const mc = new Hammer(document.body!);
+
+    const addEvent = (which : ("left" | "right"), url? : string) => {
 
         const pan = "pan" + which
-        mc.on(pan, function(e) {
+        mc.on(pan, function (e) {
 
-            if (dir[which].length > 0 && e.type === pan) 
-                window.location.assign(dir[which])
+            if (!isMoving && e.type === pan) {
+                moveBackgrounds(which)
+                //window.location.assign(url)
+            }
         })
     }
-    
+
     addEvent("left")
     addEvent("right")
- }
 
+    // if (dir.left) {
+    //     addEvent("left", dir.left)
+    // }
 
-
-
-
-
-
-const page = window.location.pathname
-
-if (page === "/index.html") {
-    swipes({
-        left: "./accueil-02.html",
-        right: ""
-    })
+    // if (dir.right) {
+    //     addEvent("right", dir.right)
+    // }
 }
-else if (page === "/accueil-02.html") {
-    swipes({
-        left: "./accueil-03.html",
-        right: "./index.html"
-    })
-}
-else if (page === "/accueil-03.html") {
-    swipes({
-        left: "./accueil-04.html",
-        right: "./accueil-02.html"
-    })
-}
-else if (page === "/accueil-04.html") {
-    swipes({
-        left: "",
-        right: "./accueil-03.html"
-    })
-}
-else if (page === "/page-02.html") {
+
+
+const page = window.location.pathname;
+let isMoving = false;
+
+swipes()
+
+if (page === "/page-02.html") {
     fabricationScroll()
 }
-    
