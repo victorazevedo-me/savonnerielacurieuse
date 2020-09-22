9066def03221bef723875b1d4613f940dfcb5acb
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-09-22 11:23:46 +0200

    :construction: Homepage swiping gestures

--- remake-parcel/src/scripts/index.ts
+++ remake-parcel/src/scripts/index.ts
@@ -1,14 +1,84 @@
 
 import fabricationScroll from './fabrication';
+import Hammer from 'hammerjs';
+
+
+
+
+
+
+
+
+function openmenu() {
+    
+// const page = window.location.pathname
+// document.body.addEventListener('wheel', function(ev: any) {
+
+//     if (ev.wheelDelta < 0) {
+//         console.log('transition')
+//     }
+
+// })
+
+}
+
+type Directions = {
+    left: string;
+    right: string;
+}
+
+ function swipes(dir: Directions): void {
+
+    const accueil = document.body!;
+    const mc = new Hammer(accueil);
+
+    const addEvent = (which: ("left" | "right")) => {
+
+        const pan = "pan" + which
+        mc.on(pan, function(e) {
+
+            if (dir[which].length > 0 && e.type === pan) 
+                window.location.assign(dir[which])
+        })
+    }
+    
+    addEvent("left")
+    addEvent("right")
+ }
+
+
+
+
+
+
 
 const page = window.location.pathname
-document.body.addEventListener('wheel', function(ev: any) {
 
-    if (ev.wheelDelta < 0) {
-        console.log('transition')
-    }
-
-})
-    
-if (page === "/page-02.html")
+if (page === "/index.html") {
+    swipes({
+        left: "./accueil-02.html",
+        right: ""
+    })
+}
+else if (page === "/accueil-02.html") {
+    swipes({
+        left: "./accueil-03.html",
+        right: "./index.html"
+    })
+}
+else if (page === "/accueil-03.html") {
+    swipes({
+        left: "./accueil-04.html",
+        right: "./accueil-02.html"
+    })
+}
+else if (page === "/accueil-04.html") {
+    swipes({
+        left: "",
+        right: "./accueil-03.html"
+    })
+}
+else if (page === "/page-02.html") {
     fabricationScroll()
+}
+    
