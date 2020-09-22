056f3abc0e422fde5484511a9c0445c85f5c2596
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-09-23 00:47:45 +0200

    :fire: Removed old files

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -26,42 +26,37 @@ function moveBackgrounds(which : ("left" | "right")): void {
     // panleft -> choose right
     // panright -> choose left
 
-    //init vals
+    // init vals
     const wrapper = document.querySelector('.backgrounds-wrapper')!;
     const current = wrapper.querySelector('.current')!;
     const index = parseInt(wrapper.id);
 
-    //init aside params
+    // init aside params
     const bckgs = ['un', 'deux', 'trois', 'quatre']
 
-    //swipe must be in backgrounds length bounds
-    if (which === "left" && index < (bckgs.length - 1) ||
-        which !== "left" && index > 0) {
-        
-        // createAside
+    // swipe must be in backgrounds length bounds
+    if (which === "left" && index < (bckgs.length - 1) || which !== "left" && index > 0) { // createAside
         const aside = document.createElement("div");
 
-        //update style & wrapper id
+        // update style & wrapper id
         if (which === "left") {
             wrapper.id = (index + 1).toString()
             aside.style.position = "100%"
             aside.className = "background aside " + bckgs[index + 1]
-            
+
         } else {
             wrapper.id = (index - 1).toString(),
             aside.style.position = "-100%"
             aside.className = "background aside " + bckgs[index - 1]
-        }
+        } wrapper.insertBefore(aside, current);
 
-        wrapper.insertBefore(aside, current);
-        
-        //isMoving pour controler animations
+        // isMoving pour controler animations
         isMoving = true
 
-        //apply animation
+        // apply animation
         aside.style.animation = (which === "left" ? "SlideLeft 1s" : "SlideRight 1s");
 
-        //reset animation controle
+        // reset animation controle
         setTimeout(() => {
             current.remove()
             aside.className = "background current " + bckgs[parseInt(wrapper.id)]
@@ -73,30 +68,22 @@ function moveBackgrounds(which : ("left" | "right")): void {
 
 function swipes(dir? : Directions): void {
 
-    const mc = new Hammer(document.body!);
+    const mc = new Hammer(document.body !);
 
     const addEvent = (which : ("left" | "right"), url? : string) => {
 
         const pan = "pan" + which
-        mc.on(pan, function (e) {
+        mc.on(pan, function(e) {
 
             if (!isMoving && e.type === pan) {
                 moveBackgrounds(which)
-                //window.location.assign(url)
+                // window.location.assign(url)
             }
         })
-    }
+    };
 
     addEvent("left")
     addEvent("right")
-
-    // if (dir.left) {
-    //     addEvent("left", dir.left)
-    // }
-
-    // if (dir.right) {
-    //     addEvent("right", dir.right)
-    // }
 }
 
 
