2a861ba3c05f899ce48011ee26f848587ecccbe6
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-09-23 23:30:25 +0200

    :art: Smooth page slides

--- src/scripts/pageControl.ts
+++ src/scripts/pageControl.ts
@@ -2,29 +2,34 @@ import Hammer from 'hammerjs';
 
 export function pageControl() {
 
-    function moveBackgrounds(panLeft: boolean): void {
+    function moveBackgrounds(): void {
 
         // init vals
         const wrapper = document.querySelector('.backgrounds-wrapper')!;
         const current = wrapper.querySelector('.current')!;
         const aside = document.createElement("div");
         const bckgs = ['un', 'deux', 'trois', 'quatre']
-        
-        // update style & wrapper id
-        aside.style.position = panLeft ? "100%" : "-100%"
+
+        const params = (page.lastIndex < page.index ? {
+            pos: "100%",
+            anim: "SlideLeft 1s"
+        } : {
+            pos: "-100%",
+            anim: "SlideRight 1s"
+        })
+
+        aside.style.position = params.pos
+        aside.style.animation = params.anim;
         aside.className = `background aside ${bckgs[page.index]}`
-        
+
         wrapper.insertBefore(aside, current);
 
-        // apply animation
-        aside.style.animation = (panLeft ? "SlideLeft 1s" : "SlideRight 1s");
-
-        // reset animation controle
+        // reset animation control
         setTimeout(() => {
             current.remove()
             aside.className = `background current ${bckgs[page.index]}`
             page.isMoving = false
-        }, 1000)    
+        }, 1000)
     }
 
     function updateTitle() {
@@ -51,47 +56,63 @@ export function pageControl() {
         }, 800)
     }
 
-    function moveDots(panLeft: boolean) {
+    function moveDots() {
 
         const dotsDOM = document.querySelectorAll('nav .dots button');
 
         dotsDOM[page.index].className = "active";
-        dotsDOM[page.index + (panLeft ? -1 : 1)].className = "";
+        dotsDOM[page.lastIndex].className = "";
     }
 
     let page = {
+
         isMoving: false,
-        index: 0
+        index: 0,
+        lastIndex: 0,
+
+        move: (newIndex: number) => {
+            page.lastIndex = page.index;
+            page.index = newIndex;
+            page.isMoving = true
+
+            moveBackgrounds()
+            moveDots()
+            updateTitle()
+        }
     }
 
+    //dots menu buttons
+    const dots = document.querySelectorAll('nav .dots button');
+    dots.forEach((elem, i) => {
+
+        elem.addEventListener('click', function() {
+
+            if (!page.isMoving && page.index !== i) {
+                let panLeft = (page.index < i)
+                page.move(i)
+            }
+        })
+    }) 
+
+    //panning events
     const emcee = new Hammer(document.body!);
+    const swipes = ["panleft", "panright"];
 
-    emcee.on(`panleft`, (e) => {
+    swipes.forEach(pan => {
 
-        if (!page.isMoving && page.index < 3) {
+        emcee.on(pan, () => {
 
-            page.isMoving = true;
-            page.index += 1;
-            moveBackgrounds(true)
-            moveDots(true)
-            updateTitle()
+            if (!page.isMoving) {
 
-            console.log(page.index)
-        }
-    })
+                if (pan === "panleft" && page.index < 3) {
+                    page.move(page.index + 1)
+                }
 
-    emcee.on(`panright`, (e) => {
-
-        if (!page.isMoving && page.index > 0) {
-
-            page.isMoving = true;
-            page.index -= 1;
-            moveBackgrounds(false)
-            moveDots(false)
-            updateTitle()
-
-            console.log(page.index)
-        }
+                if (pan === "panright" && page.index > 0) {
+                    page.move(page.index - 1)
+                }
+            }
+        })
     })
 }
 
