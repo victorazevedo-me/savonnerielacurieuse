7e96103f0c793cde18e7ede02b49d99bb8277452
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-09-24 19:15:28 +0200

    :art: Added page 2 footer, tweaked style

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,4 +1,11 @@
 import pageControl from './pageControl';
+import fabricationScroll from './fabrication';
+
+/*
+ *
+ * Function Cemetary
+ *
+*/
 
 function openmenu() {
 
@@ -15,16 +22,37 @@ function openmenu() {
 
 function customCursor() {
     
-    const cursor = document.querySelector('#cursor')!;
-    const width = cursor.scrollWidth;
+    // const cursor = document.querySelector('#cursor')!;
+    // const width = cursor.scrollWidth;
 
-    document.addEventListener('mousemove', (e) => {
-        cursor.setAttribute('style', `top: ${e.pageY - width / 2}px; left: ${e.pageX - width / 2}px`)
-    })
+    // document.addEventListener('mousemove', (e) => {
+    //     cursor.setAttribute('style', `top: ${e.pageY - width / 2}px; left: ${e.pageX - width / 2}px`)
+    // })
+}
+
+
+
+
+
+
+function overlayPosition(overlay: string, targetElem: string, posY: number):void {
+
+    const bodyTop = document.body.getBoundingClientRect().top;
+    const elemTop = document.querySelector(targetElem)!.getBoundingClientRect().top;
+    const overlayDOM = (<HTMLElement>document.querySelector(overlay));
+
+    overlayDOM.style.top = `${(elemTop - bodyTop) + (posY - overlayDOM.offsetHeight / 2)}px`
 }
 
 
 window.onload = function() {
-    pageControl()
-    //customCursor()
+
+    if (window.location.pathname === "/index.html") {
+        pageControl()
+        //customCursor()
+    }
+    else if (window.location.pathname === "/page-02.html") {
+        fabricationScroll()
+        overlayPosition('.grosseballe', '.expliquation', 500)
+    }
 }
