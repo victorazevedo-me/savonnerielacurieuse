1d40331a3ddd25a8c904fc82888d2c249dda9fbc
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-09-24 12:50:28 +0200

    :art: Updated accueil style & footer

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -13,6 +13,18 @@ function openmenu() {
 
 }
 
+function customCursor() {
+    
+    const cursor = document.querySelector('#cursor')!;
+    const width = cursor.scrollWidth;
+
+    document.addEventListener('mousemove', (e) => {
+        cursor.setAttribute('style', `top: ${e.pageY - width / 2}px; left: ${e.pageX - width / 2}px`)
+    })
+}
+
+
 window.onload = function() {
     pageControl()
+    //customCursor()
 }
