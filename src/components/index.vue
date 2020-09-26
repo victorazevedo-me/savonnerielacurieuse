1d5c4e16ad27176f849363a3e727e91f06af25e9
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-09-26 19:53:13 +0200

    :truck: Accueil to *.vue

---
+++ src/components/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <section id="contenu-page">
+        <div class="backgrounds-wrapper">
+            <div class="background aside deux"></div>
+            <div class="background current un"></div>
+        </div>
+
+        <div class="accueil">
+
+            <div class="titre">
+
+                <div class="big">
+                    <div>
+                        <span>Savonnerie</span>
+                    </div>
+                    <div>
+                        <span>La Curieuse</span>
+                    </div>
+                </div>
+
+                <div class="small">
+                    <div>
+                        <span>création de savons écologiques</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
