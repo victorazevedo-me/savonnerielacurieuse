d0abb6c2f3b15cbf4cc4d4043c2e7c7dc283f3b3
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-09-30 14:59:41 +0200

    :art: Added global animation timeout

--- src/scripts/accueilControl.ts
+++ src/scripts/accueilControl.ts
@@ -1,83 +1,79 @@
-
 function customCursor() {
-    
-    // const cursor = document.querySelector('#cursor')!;
-    // const width = cursor.scrollWidth;
-
-    // document.addEventListener('mousemove', (e) => {
-    //     cursor.setAttribute('style', `top: ${e.pageY - width / 2}px; left: ${e.pageX - width / 2}px`)
-    // })
+	// const cursor = document.querySelector('#cursor')!;
+	// const width = cursor.scrollWidth;
+	// document.addEventListener('mousemove', (e) => {
+	//     cursor.setAttribute('style', `top: ${e.pageY - width / 2}px; left: ${e.pageX - width / 2}px`)
+	// })
 }
 
 export function accueilSwipe(u: string, i: number, lasti?: number) {
+	let url = u.replace('/', '')
 
-    let url = u.replace('/', '');
+	function moveBackgrounds() {
+		// init vals
+		const wrapper = document.querySelector('.background-wrapper')!
+		const current = wrapper.querySelector('.background-wrapper div')!
+		const next = document.createElement('div')
 
-    function moveBackgrounds(): void {
+		next.classList.add('background', url)
+		current.classList.add('fadeout')
+		wrapper.appendChild(next)
 
-        // init vals
-        const wrapper = document.querySelector('.background-wrapper')!;
-        const current = wrapper.querySelector('.background-wrapper div')!;
-        const next = document.createElement('div');
-    
-        next.classList.add('background', url);
-        current.classList.add('fadeout');
-        wrapper.appendChild(next);
+		document.body.style.overflowY = 'none'
 
-        document.body.style.overflowY = 'none';
+		setTimeout(() => {
+			current.remove()
+			next.classList.add('front')
+			document.body.style.overflowY = 'scroll'
+		}, 1200)
+	}
 
-        setTimeout(() => {
-            current.remove()
-            next.classList.add('front');
-            document.body.style.overflowY = 'scroll';
-        }, 1200)
-    }
+	function updateTitle() {
+		const texts = [
+			{
+				titre: ['Savonnerie', 'La Curieuse'],
+				soustitre: 'création de savons écologiques'
+			},
+			{ titre: ['la saponification', 'à froid'], soustitre: '' },
+			{ titre: ['César et les', 'savons doux'], soustitre: '' },
+			{ titre: ['Où les', 'trouver ?'], soustitre: '' }
+		]
 
-    function updateTitle() {
+		const soustitre = document.querySelector('.small div span')!
+		const spans = document.querySelectorAll('.titre span')!
 
-        const texts = [
-            {titre: ["Savonnerie", "La Curieuse"], soustitre: "création de savons écologiques"},
-            {titre: ["la saponification", "à froid"], soustitre: ""},
-            {titre: ["César et les", "savons doux"], soustitre: ""},
-            {titre: ["Où les", "trouver ?"], soustitre: ""}
-        ];
+		const swipeAnim = (dom: Element, duration: number) => {
+			const animname =
+				typeof lasti !== 'number'
+					? 'init'
+					: lasti > i
+					? 'swipedRight'
+					: 'swipedLeft'
+			dom.classList.add(animname)
 
-        const soustitre = document.querySelector('.small div span')!;
-        const spans = document.querySelectorAll('.titre span')!;
-        
-        const swipeAnim = (dom: Element, duration: number) => {
+			setTimeout(() => {
+				dom.classList.remove(animname)
+			}, duration)
+		}
 
-            const animname = (typeof(lasti) !== "number" ? 'init' : (lasti > i ? 'swipedRight' : 'swipedLeft'));
-            dom.classList.add(animname)
-        
-            return new Promise(resolve => {
-                setTimeout(() => {
-                    dom.classList.remove(animname)
-                    resolve(true)
-                }, duration);
-            });
-        }        
+		function addText(): void {
+			document.querySelectorAll('.big div span')!.forEach((span, j) => {
+				span.innerHTML = texts[i].titre[j]
+			})
+			soustitre.innerHTML = texts[i].soustitre
+		}
 
-        function addText(): void {
+		spans.forEach(span => {
+			if (typeof lasti === 'number') {
+				swipeAnim(span, 1200)
+				setTimeout(addText, 600)
+			} else {
+				swipeAnim(span, 1200)
+				addText()
+			}
+		})
+	}
 
-            document.querySelectorAll('.big div span')!.forEach((span, j) => {
-                span.innerHTML = texts[i].titre[j]
-            })
-            soustitre.innerHTML = texts[i].soustitre
-        }
-    
-        spans.forEach(span => {
-
-            if (typeof(lasti) === 'number') {
-                swipeAnim(span, 1200).then((r) => console.log(r))
-                setTimeout(addText, 600)
-            } else {
-                swipeAnim(span, 600)
-                addText()
-            }
-        })
-    }
-
-    moveBackgrounds();
-    updateTitle();
+	moveBackgrounds()
+	updateTitle()
 }
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,179 +1,190 @@
-import { openPage } from './pageControl';
-import { accueilSwipe } from './accueilControl';
-import Index from '../components/index.vue';
+import { openPage } from './pageControl'
+import { accueilSwipe } from './accueilControl'
+import Index from '../components/index.vue'
 
-import Hammer from 'hammerjs';
-import Vue from 'vue';
+import Hammer from 'hammerjs'
+import Vue from 'vue'
 
+const SITEMAP = [
+	{
+		pathname: '/la-savonnerie',
+		titre: 'La savonnerie'
+	},
+	{
+		pathname: '/la-saponification',
+		titre: 'La saponification'
+	},
+	{
+		pathname: '/les-savons',
+		titre: 'Les savons'
+	},
+	{
+		pathname: '/ou-les-trouver',
+		titre: 'Où les trouver'
+	}
+]
 
-const SITEMAP = [{
-    pathname: "/la-savonnerie",
-    titre: "La savonnerie"
-},{
-    pathname: "/la-saponification",
-    titre: "La saponification"
-},{
-    pathname: "/les-savons",
-    titre: "Les savons"
-},{
-    pathname: "/ou-les-trouver",
-    titre: "Où les trouver"
+enum PageEventOrigin {
+	initialisation,
+	homepageScroll,
+	headerLogo
 }
-];
 
-enum PageEventOrigin { initialisation, homepageScroll, headerLogo }
+function getPageIndex(): number {
+	let result = -1
 
-const getPageIndex = (): number => {
+	SITEMAP.forEach((u, i) => {
+		if (u.pathname === window.location.pathname) result = i
+	})
 
-    let result = -1;
-
-    SITEMAP.forEach((u, i) => {
-        if (u.pathname === window.location.pathname) result = i
-    })
-
-    if (result < 0) {
-        console.warn(Error('URL not in the list'))
-        return 0
-    } else {
-        return result
-    }
+	if (result < 0) {
+		console.warn('getPageIndex defaulted to 0')
+		return 0
+	} else {
+		return result
+	}
 }
 
 function accueilEvents() {
+	enum AnimationIs {
+		Playing,
+		Finished
+	}
+	const anim = {
+		state: AnimationIs.Finished,
 
-    //Object
-    const Nav = {
+		wait: () => {
+			anim.state = AnimationIs.Playing
+			setTimeout(() => {
+				anim.state = AnimationIs.Finished
+			}, 1300)
+		}
+	}
 
-        dom: document.querySelector('body > nav')!,
-        centerLis: document.querySelectorAll('nav .center li'),
+	//Object
+	const Nav = {
+		dom: document.querySelector('body > nav')!,
+		centerLis: document.querySelectorAll('nav .center li'),
 
-        changeCenterFocus: (i: number) => {  
-            Nav.centerLis.forEach(n => n.classList.remove('active'))
-            Nav.centerLis[i].classList.add('active')
-        }
-    };  
+		changeCenterFocus: (i: number) => {
+			Nav.centerLis.forEach(n => n.classList.remove('active'))
+			Nav.centerLis[i].classList.add('active')
+		}
+	}
 
-    const navButtons = () => {
-        
-        Nav.centerLis.forEach((elem, i) => {
-    
-            elem.addEventListener('click', function() {
-    
-                const currentPage = getPageIndex()
-    
-                if (currentPage !== i) {
-                    redirection(PageEventOrigin.homepageScroll, i);
-                    Nav.changeCenterFocus(i);
-                }
-            })
-        });
-    }
+	const navButtons = () => {
+		Nav.centerLis.forEach((elem, i) => {
+			elem.addEventListener('click', function() {
+				//only do animation if not playing
 
-    const panningScroll = () => {
-        const emcee = new Hammer(document.body!);
-        const swipes = ["panleft", "panright"];
-        let swipeMoves = {
-            max: 100,
-            now: 0
-        }
-        
-        swipes.forEach(pan => {
+				const currentPage = getPageIndex()
 
-            emcee.on(pan, () => {
+				if (anim.state === AnimationIs.Finished && currentPage !== i) {
+					anim.wait()
+					redirection(PageEventOrigin.homepageScroll, i)
+					Nav.changeCenterFocus(i)
+				}
+			})
+		})
+	}
 
-                const currentIndex = getPageIndex()
+	const panningScroll = () => {
+		const emcee = new Hammer(document.body!)
+		const swipes = ['panleft', 'panright']
 
-                //pour eviter de swipe n'importe quand
-                //à corriger en définissant finishposX - startposX
-                if (swipeMoves.now < swipeMoves.max) {
-                    swipeMoves.now += 1
-                } else {
+		//pour eviter de swipe n'importe quand
+		//à corriger en définissant finishposX - startposX
 
-                    swipeMoves.now = 0
+		swipes.forEach(pan => {
+			emcee.on(pan, () => {
+				//only do animation if not playing
+				if (anim.state === AnimationIs.Finished) {
+					const currentIndex = getPageIndex()
 
-                    //deplace l'accueil droite et gauche
-                    if (pan === "panleft" && currentIndex < 3) {
-                        redirection(PageEventOrigin.homepageScroll, currentIndex + 1)
-                    }
+					const applyPan = (i: number) => {
+						anim.wait()
+						redirection(PageEventOrigin.homepageScroll, i)
+						Nav.changeCenterFocus(i)
+					}
 
-                    if (pan === "panright" && currentIndex > 0) {
-                        redirection(PageEventOrigin.homepageScroll, currentIndex - 1)
-                    }
+					//deplace l'accueil droite et gauche
+					if (pan === 'panleft' && currentIndex < 3) {
+						applyPan(currentIndex + 1)
+					}
 
-                    Nav.changeCenterFocus(currentIndex);
-                }
-            })
-        });
-    }
-    
-    const hamburger = () => {
-        const hamburger = document.querySelector('.hamburger')!;
-        hamburger.addEventListener('click', () => {
+					if (pan === 'panright' && currentIndex > 0) {
+						applyPan(currentIndex - 1)
+					}
+				}
+			})
+		})
+	}
 
-            if (hamburger.classList.contains('clicked')) {
-                hamburger.classList.remove('clicked')
-            } else {
-                //new Vue({el: "#extended-nav-content", template: '<ExtendedNav />', components: { ExtendedNav }})
-                hamburger.classList.add('clicked')
-            }
-            
-        })
-    }
+	const hamburger = () => {
+		const hamburger = document.querySelector('.hamburger')!
+		hamburger.addEventListener('click', () => {
+			if (hamburger.classList.contains('clicked')) {
+				hamburger.classList.remove('clicked')
+			} else {
+				//new Vue({el: "#extended-nav-content", template: '<ExtendedNav />', components: { ExtendedNav }})
+				hamburger.classList.add('clicked')
+			}
+		})
+	}
 
-    const verticalScroll = () => {
+	const verticalScroll = () => {
+		document.body.addEventListener('wheel', function(ev: any) {
+			if (ev.wheelDelta < 0) {
+			} else if (ev.wheelDelta > 0) {
+			}
+		})
+	}
 
-        document.body.addEventListener('wheel', function(ev: any) {
+	const headerLogo = () => {
+		const logo = document.querySelector('nav .logo')!
+		logo.addEventListener('click', () => {
+			//Accueil.arrive(PageEventOrigin.headerLogo)
+		})
+	}
 
-            if (ev.wheelDelta < 0) {
-            }
-            else if (ev.wheelDelta > 0) {
-            }
-        })
-    }
-    
-    const headerLogo = () => {
-        const logo = document.querySelector('nav .logo')!;
-        logo.addEventListener('click', () => {
-            //Accueil.arrive(PageEventOrigin.headerLogo)
-        })
-    }
-    
-    //verticalScroll()
-    navButtons()
-    panningScroll()
-    hamburger()
-    //headerLogo()
+	//verticalScroll()
+	navButtons()
+	panningScroll()
+	hamburger()
+	//headerLogo()
 }
 
 function redirection(which: PageEventOrigin, index?: number) {
+	if (which === PageEventOrigin.initialisation) {
+		new Vue({
+			el: '#contenu-accueil',
+			template: '<Index />',
+			components: { Index }
+		})
+		accueilEvents()
 
-    if (which === PageEventOrigin.initialisation) {
-
-        new Vue({el: "#contenu-accueil", template: '<Index />', components: { Index }});
-        accueilEvents();
-
-        if (window.location.pathname.length > 1) {
-            const i = getPageIndex();
-            accueilSwipe(SITEMAP[i].pathname, i);
-            openPage(i)
-        }
-    }
-
-    else if (which === PageEventOrigin.homepageScroll && typeof(index) === 'number') {
-
-        const lastIndex = getPageIndex()
-        window.history.pushState("localhost:1234", SITEMAP[index].titre, SITEMAP[index].pathname);
-        accueilSwipe(SITEMAP[index].pathname, index, lastIndex);
-        openPage(index);
-    }
-
-    else if (which === PageEventOrigin.headerLogo) {
-        //appendPage()
-    }
+		if (window.location.pathname.length > 1) {
+			const i = getPageIndex()
+			accueilSwipe(SITEMAP[i].pathname, i)
+			openPage(i)
+		}
+	} else if (
+		which === PageEventOrigin.homepageScroll &&
+		typeof index === 'number'
+	) {
+		const lastIndex = getPageIndex()
+		window.history.pushState(
+			'localhost:1234',
+			SITEMAP[index].titre,
+			SITEMAP[index].pathname
+		)
+		accueilSwipe(SITEMAP[index].pathname, index, lastIndex)
+		openPage(index)
+	} else if (which === PageEventOrigin.headerLogo) {
+	}
 }
 
 window.onload = function() {
-
-    // directories control
-    redirection(PageEventOrigin.initialisation)
+	// directories control
+	redirection(PageEventOrigin.initialisation)
 }
