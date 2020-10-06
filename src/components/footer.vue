1e3140b6532ceb007f730daec91af53ad1152518
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-10-06 07:36:11 +0200

    :art: Footer now links

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,42 +1,25 @@
 <template>
-    <footer id="footer">
+	<footer id="footer">
+		<hr />
 
-            <hr />
-        
-            <div class="wrapper">
-                <div class="navigation">
-                    <div class="parent">
-                        <li>
-                            <span>Accueil</span>
-                        </li>
-                        <li>
-                            <span>la saponification à froid</span>
-                        </li>
-                        <li>
-                            <span>César et les savons doux</span>
-                        </li>
-                        <li>
-                            <span>où les trouver</span>
-                        </li>
-                    </div>
-                    <div class="child">
-                        <li>Boutique & marchés</li>
-                        <li>Evenements</li>
-                        <li></li>
-                    </div>
-                </div>
-                <div>
-                    <div class="contact">
-                        <p>savonnerielacurieuse@gmail.com</p>
-                        <p>06 89 56 45 12</p>
-                        <p class="adresse">
-                            <span>Rue du Château</span>
-                            <span>63120 Vollore-Ville</span>
-                            <span>Puy-de-Dôme, France</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        
-        </footer>
+		<div class="wrapper">
+			<div class="navigation">
+				<a href="/">Accueil</a>
+				<a href="/la-saponification">La saponification à froid</a>
+				<a href="/les-savons">César et les savons doux</a>
+				<a href="/ou-les-trouver">Où les trouver</a>
+			</div>
+			<div>
+				<div class="contact">
+					<p>savonnerielacurieuse@gmail.com</p>
+					<p>06 89 56 45 12</p>
+					<p class="adresse">
+						<span>Rue du Château</span>
+						<span>63120 Vollore-Ville</span>
+						<span>Puy-de-Dôme, France</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer>
 </template>
